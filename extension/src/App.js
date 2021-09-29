1929ffcc3f5443d33a7a5ef3e7aec28def950e26
ChrisCruze <cruzc09@gmail.com>
2021-09-29 19:34:18 -0400

    connected API

--- extension/src/App.js
+++ extension/src/App.js
@@ -20,17 +20,44 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Autocomplete from "@mui/material/Autocomplete";
+import axios from "axios";
 
 // import Image from "material-ui-image";
 
 const theme = createTheme();
 
 const selectFieldDict = {
-  origin: { options: [{ label: "IAD", value: "IAD" }], label: "Origin", id: "origin" },
-  destination: { options: [{ label: "IAD", value: "IAD" }], label: "Destination", id: "destination" },
-  airline: { options: [{ label: "IAD", value: "IAD" }], label: "Airline", id: "airline" }
+  origin: { options: [{ label: "MEM", value: "MEM" }], label: "Origin", id: "origin" },
+  destination: { options: [{ label: "IAH", value: "IAH" }], label: "Destination", id: "destination" },
+  airline: { options: [{ label: "UA", value: "UA" }], label: "Airline", id: "airline" }
 };
 
+const getFlightDelay = ({ setResponse, url }) => {
+  //const url = `https://e553-34-83-235-2.ngrok.io/delay_frequency/origin=MEM&destination=IAH&airline=UA`;
+  const options = {
+    url: url,
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+      "Access-Control-Allow-Origin": "*"
+    }
+  };
+
+  axios(options).then(response => {
+    const responseResult = { data: response.data, status: response.status };
+    console.log({ responseResult });
+    setResponse(responseResult);
+  });
+};
+
+const createURLFromForm = ({ formData }) => {
+  const origin = formData.origin.value;
+  const destination = formData.destination.value;
+  const airline = formData.airline.value;
+  const url = `https://e553-34-83-235-2.ngrok.io/delay_frequency/origin=${origin}&destination=${destination}&airline=${airline}`;
+  return url;
+};
 const App = () => {
   const [url, setUrl] = useState("");
   const [formData, setFormdata] = useState({
@@ -38,9 +65,12 @@ const App = () => {
     destination: { value: "", label: "" },
     airline: { value: "", label: "" }
   });
+  const [response, setResponse] = useState({ data: undefined, response: "" });
   const handleSubmit = event => {
     event.preventDefault();
     console.log({ formData });
+    const url = createURLFromForm({ formData });
+    getFlightDelay({ setResponse, url });
   };
 
   useEffect(() => {
@@ -97,7 +127,7 @@ const App = () => {
             <Avatar alt="Remy Sharp" src="/images/suit_case_logo.ico" />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Flight Advisor
+            {response.data || "Flight Advisor"}
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
