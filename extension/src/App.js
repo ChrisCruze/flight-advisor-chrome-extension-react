bb9c2b6acf29a42eaf66f6d5a297f05bd065b3ce
ChrisCruze <cruzc09@gmail.com>
2021-10-03 10:26:10 -0400

    integrated speedometer

--- extension/src/App.js
+++ extension/src/App.js
@@ -21,19 +21,35 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Autocomplete from "@mui/material/Autocomplete";
 import axios from "axios";
+import GaugeChart from "react-gauge-chart";
 
 // import Image from "material-ui-image";
 
 const theme = createTheme();
 
+const ngrok_url = "https://1798-34-83-40-166.ngrok.io/";
 const selectFieldDict = {
   origin: { options: [{ label: "MEM", value: "MEM" }], label: "Origin", id: "origin" },
   destination: { options: [{ label: "IAH", value: "IAH" }], label: "Destination", id: "destination" },
   airline: { options: [{ label: "UA", value: "UA" }], label: "Airline", id: "airline" }
 };
 
+//https://www.npmjs.com/package/react-gauge-chart
+const SpeedometerChart = ({ percent }) => {
+  return (
+    <GaugeChart
+      id="gauge-chart5"
+      textColor="black"
+      nrOfLevels={420}
+      arcsLength={[0.3, 0.5, 0.2]}
+      colors={["#5BE12C", "#F5CD19", "#EA4228"]}
+      percent={percent || 0}
+      arcPadding={0.02}
+    />
+  );
+};
+
 const getFlightDelay = ({ setResponse, url }) => {
-  //const url = `https://e553-34-83-235-2.ngrok.io/delay_frequency/origin=MEM&destination=IAH&airline=UA`;
   const options = {
     url: url,
     method: "GET",
@@ -43,7 +59,6 @@ const getFlightDelay = ({ setResponse, url }) => {
       "Access-Control-Allow-Origin": "*"
     }
   };
-
   axios(options).then(response => {
     const responseResult = { data: response.data, status: response.status };
     console.log({ responseResult });
@@ -55,33 +70,23 @@ const createURLFromForm = ({ formData }) => {
   const origin = formData.origin.value;
   const destination = formData.destination.value;
   const airline = formData.airline.value;
-  const url = `https://e553-34-83-235-2.ngrok.io/delay_frequency/origin=${origin}&destination=${destination}&airline=${airline}`;
+  const url = `${ngrok_url}/delay_frequency/origin=${origin}&destination=${destination}&airline=${airline}`;
   return url;
 };
 const App = () => {
-  const [url, setUrl] = useState("");
   const [formData, setFormdata] = useState({
     origin: { value: "", label: "" },
     destination: { value: "", label: "" },
     airline: { value: "", label: "" }
   });
   const [response, setResponse] = useState({ data: undefined, response: "" });
+
   const handleSubmit = event => {
     event.preventDefault();
     console.log({ formData });
     const url = createURLFromForm({ formData });
     getFlightDelay({ setResponse, url });
   };
-
-  useEffect(() => {
-    const queryInfo = { active: true, lastFocusedWindow: true };
-    chrome.tabs &&
-      chrome.tabs.query(queryInfo, tabs => {
-        const url = tabs[0].url;
-        setUrl(url);
-      });
-  }, []);
-
   const TypeAheadSelect = ({ options, label, id }) => {
     return (
       <Autocomplete
@@ -127,7 +132,8 @@ const App = () => {
             <Avatar alt="Remy Sharp" src="/images/suit_case_logo.ico" />
           </Avatar>
           <Typography component="h1" variant="h5">
-            {response.data || "Flight Advisor"}
+            Flight Advisor
+            {/* {response.data || "Flight Advisor"} */}
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
@@ -147,6 +153,11 @@ const App = () => {
             <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
               Submit
             </Button>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <SpeedometerChart percent={response.data} />
+              </Grid>
+            </Grid>
           </Box>
         </Box>
       </Container>
